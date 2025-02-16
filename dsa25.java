2a9987a699243499a7dd2f35050c113ed1b98104
gitvivek123-hub <vivekwakchaure87@gmail.com>
2025-02-16 21:12:44 +0530

    Create dsa25.java

---
+++ dsa25.java
@@ -0,0 +1,25 @@
+import java.util.Stack;
+
+class Solution {
+    public boolean isValid(String s) {
+       
+        Stack<Character> stack = new Stack<>();
+       
+        for (char c : s.toCharArray()) {
+            
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else {
+                
+                if (stack.isEmpty() || 
+                   (c == ')' && stack.pop() != '(') || 
+                   (c == '}' && stack.pop() != '{') || 
+                   (c == ']' && stack.pop() != '[')) {
+                    return false;
+                }
+            }
+        }
+       
+        return stack.isEmpty();
+    }
+}
